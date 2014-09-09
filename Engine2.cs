303442c8bb489551e353057e37e1ca9467cb6811
Jason Crease <jason.crease@red-gate.com>
2014-09-10 00:04:17 +0100

    a few more colors and graphics

--- Engine2.cs
+++ Engine2.cs
@@ -10,13 +10,22 @@ namespace FoosballAngles
     class Engine2
     {
         public System.Drawing.Bitmap Bitmap { get; set; }
+        public int imageWidth = 750;
+        public int imageHeight = 350;
+        public int goalWidth = 200;
 
         internal void Go()
         {
-            Bitmap = new System.Drawing.Bitmap(750, 350);
+            Bitmap = new System.Drawing.Bitmap(imageWidth, imageHeight);
             Graphics g = Graphics.FromImage(Bitmap);
-            Brush brush = new SolidBrush(Color.BlueViolet);
-            g.DrawLine(new Pen(brush), 100, 200, 300, 400);
+            Brush greenBrush = new SolidBrush(Color.DarkGreen);
+            Brush yellowBrush = new SolidBrush(Color.DarkGreen);
+            Brush goalBrush = new SolidBrush(Color.BlueViolet);
+            g.FillRectangle(greenBrush, 0, 0, imageWidth, imageHeight);
+            g.FillRectangle(goalBrush, 0, (imageHeight - goalWidth) /2, 8, goalWidth);
+
+
+            //g.DrawLine(new Pen(brush), 100, 200, 300, 400);
         }
     }
 }
